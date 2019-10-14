ad4ffe09d8f54286b6063f0758236d5bc004ff9d
Dr Ocelot <19556715+doktorocelot@users.noreply.github.com>
2019-10-14 13:26:31 -0400

    more stuff for menu proto

--- app.js
+++ app.js
@@ -1,15 +1,87 @@
-import Game from './script/test/game.js';
+import Menu from './script/menu.js';
+import Input from './script/input.js';
 
-let game = null;
-export let test = 'CHEESE';
-function newGame(type) {
-  game = new Game();
+const menu = new Menu();
+const input = new Input();
+document.addEventListener('DOMContentLoaded', () => {
+  menu.load('root');
+  menu.show();
+});
+function $(selector) {
+  const selection = document.querySelectorAll(selector);
+  switch (selection.length) {
+    case 0:
+      return undefined;
+      break;
+    case 1:
+      return selection[0];
+      break;
+    default:
+      return selection;
+  }
 }
-newGame('marathon');
-game.start();
-setTimeout(() => {
-  test = 'MONEY';
-}, 1000);
-setTimeout(() => {
-  game.start();
-}, 2000);
+
+function hideHints() {
+  $('#button-hint-container').classList.add('hidden');
+}
+function showHints() {
+  $('#button-hint-container').classList.remove('hidden');
+}
+function changeHints(type) {
+  switch (type) {
+    case 'keyboard':
+      $('#select-button').src = 'img/buttons/keyboard-arrows.svg';
+      $('#confirm-button').src = 'img/buttons/keyboard-enter.svg';
+      $('#back-button').src = 'img/buttons/keyboard-backspace.svg';
+      break;
+    default: {
+      $('#select-button').src = 'img/buttons/general-dpad.svg';
+      $('#confirm-button').src = 'img/buttons/general-a.svg';
+      $('#back-button').src = 'img/buttons/general-b.svg';
+    }
+  }
+}
+let mouseLimit = 0;
+document.addEventListener('keydown', (event) => {
+  document.body.requestPointerLock();
+  mouseLimit = 0;
+  showHints();
+  changeHints('keyboard');
+  if (event.keyCode === 38) {
+    menu.up();
+  }
+  if (event.keyCode === 40) {
+    menu.down();
+  }
+  if (event.keyCode === 13) {
+    menu.ok();
+  }
+  if (event.keyCode === 8) {
+    menu.back();
+  }
+});
+
+document.addEventListener('mousemove', (e) => {
+  mouseLimit++;
+
+  if (mouseLimit > 3) {
+    document.exitPointerLock();
+    hideHints();
+  }
+});
+const gamepad = new Gamepad();
+gamepad.bind(Gamepad.Event.BUTTON_DOWN, (e) => {
+  // e.control of gamepad e.gamepad pressed down
+  console.log(e.control);
+  showHints();
+  changeHints('controller');
+  if (e.control === 'DPAD_UP') {
+    menu.up();
+  }
+  if (e.control === 'DPAD_DOWN') {
+    menu.down();
+  }
+});
+if (!gamepad.init()) {
+  // Your browser does not support gamepads, get the latest Google Chrome or Firefox
+}
