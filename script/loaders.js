e6930b4f0e6c538081477d1ec57075c795665109
Dr-Ocelot <zacharylohrman@gmail.com>
2019-10-14 14:57:44 -0400

    true fix

--- script/loaders.js
+++ script/loaders.js
@@ -1,8 +1,8 @@
 export function loadGameType(name) {
-  return fetch(`../gametypes/${name}.json`)
+  return fetch(`./gametypes/${name}.json`)
       .then((r) => r.json());
 }
 export function loadMenu(name) {
-  return fetch(`../menus/${name}.json`)
+  return fetch(`./menus/${name}.json`)
       .then((r) => r.json());
 }
