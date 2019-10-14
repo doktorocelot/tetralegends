be6e7f8bcb1459fcc2aa9ce44e597aa7ee3c1adb
Dr-Ocelot <zacharylohrman@gmail.com>
2019-10-14 14:52:46 -0400

    menu prototype advances

---
+++ script/menu/button-hints.js
@@ -0,0 +1,26 @@
+import $ from '../shortcuts.js';
+class ButtonHints {
+  constructor() {}
+  change(type) {
+    switch (type) {
+      case 'keyboard':
+        $('#select-button').src = 'img/buttons/keyboard-arrows.svg';
+        $('#confirm-button').src = 'img/buttons/keyboard-enter.svg';
+        $('#back-button').src = 'img/buttons/keyboard-backspace.svg';
+        break;
+      default: {
+        $('#select-button').src = 'img/buttons/general-dpad.svg';
+        $('#confirm-button').src = 'img/buttons/general-a.svg';
+        $('#back-button').src = 'img/buttons/general-b.svg';
+      }
+    }
+  }
+  hide() {
+    $('#button-hint-container').classList.add('hidden');
+  }
+  show() {
+    $('#button-hint-container').classList.remove('hidden');
+  }
+}
+const buttonHints = new ButtonHints;
+export default buttonHints;
