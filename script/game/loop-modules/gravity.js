df4630793cfba45a90e33717293d7794289b70c4
Dr Ocelot <19556715+doktorocelot@users.noreply.github.com>
2019-12-01 18:44:16 -0500

    temporary disable of particles and bg

--- script/game/loop-modules/gravity.js
+++ script/game/loop-modules/gravity.js
@@ -1,6 +1,25 @@
-import {fallen} from './shared/gravity.js';
+import gameHandler from '../game-handler.js';
+import {SCORE_TABLES} from '../../consts.js';
+import sound from '../../sound.js';
 
-export default function gravity(arg) {
+function fallen(piece) {
+  if (piece.yFloor > Math.floor(piece.lastY)) {
+    if (piece.gravityMultiplier !== 1 || piece.gravityOverride) {
+      for (let i = 1; i <= (piece.yFloor - Math.floor(piece.lastY)); i++) {
+        {gameHandler.game.addScore('softDrop');}
+      }
+      if (SCORE_TABLES[gameHandler.game.settings.scoreTable].updateSoftDropImmediately) {
+        gameHandler.game.updateStats();
+      }
+    }
+    piece.isDirty = true;
+    if (piece.isLanded) {
+      sound.add('step');
+    }
+  }
+}
+
+export function gravity(arg) {
   const piece = arg.piece;
   let distance = arg.ms / (piece.gravity / piece.gravityMultiplier);
   if (piece.gravityOverride) {
@@ -14,6 +33,21 @@ export default function gravity(arg) {
     }
   } else {
     piece.y = Math.floor(piece.y);
+  }
+  fallen(piece);
+}
+export function classicGravity(arg) {
+  const piece = arg.piece;
+  let distance = arg.ms / (piece.gravity / piece.gravityMultiplier);
+  if (piece.gravityOverride) {
+    distance = arg.ms / piece.gravityOverride;
+  }
+  const oldY = piece.y;
+  piece.y += Math.min(distance);
+  if (piece.isStuck) {
+    piece.y = oldY;
+    piece.sonicDrop();
+    piece.mustLock = true;
   }
   fallen(piece);
 }
