408e9d5ab369e053684ef0800ea45fabac1685c7
Dr Ocelot <19556715+doktorocelot@users.noreply.github.com>
2020-03-20 01:39:55 -0400

    Update settings.js

--- script/settings.js
+++ script/settings.js
@@ -1,7 +1,7 @@
 import menu from './menu/menu.js';
 import sound from './sound.js';
 import locale from './lang.js';
-const SETTINGS_VERSION = 4;
+const SETTINGS_VERSION = 5;
 class Settings {
   constructor() {
     this.defaultSettings = {
