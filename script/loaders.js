07a7422b075791c8ee596750e00b428bd057d51a
Dr Ocelot <19556715+doktorocelot@users.noreply.github.com>
2020-03-20 09:36:50 -0400

    font optimizations

--- script/loaders.js
+++ script/loaders.js
@@ -1,16 +1,16 @@
 export function loadGameType(name) {
-  return fetch(`./gametypes/${name}.json`)
+  return fetch(`./gametypes/${name}.json`, {cache: 'no-store'})
       .then((r) => r.json());
 }
 export function loadMenu(name) {
-  return fetch(`./ui/${name}.json`)
+  return fetch(`./ui/${name}.json`, {cache: 'no-store'})
       .then((r) => r.json());
 }
 export function loadSoundbank(name) {
-  return fetch(`./se/game/${name}/info.json`)
+  return fetch(`./se/game/${name}/info.json`, {cache: 'no-store'})
       .then((r) => r.json());
 }
 export function loadLanguage(name, file) {
-  return fetch(`./lang/${name}/${file}.json`)
+  return fetch(`./lang/${name}/${file}.json`, {cache: 'no-store'})
       .then((r) => r.json());
 }
